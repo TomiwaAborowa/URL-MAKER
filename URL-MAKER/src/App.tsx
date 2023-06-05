911c237ad92f0d68cb9f8e539743d1ca4186e3b9
Vee-keme <veliaprecious@gmail.com>
2023-06-05 01:00:43 +0100

    project template with react+typescript+tailwindcss

---
+++ URL-MAKER/src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen rounded-lg flex justify-center items-center">
+        <div className="bg-slate-600 w-[80%] h-[30%] rounded-2xl flex items-center justify-center">
+          <h1 className="text-blue-400 text-5xl">PROJECT INITIALISED</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
